dcaf04bec5f37bf39027b0077ac4b09206b512ca
albertocastelli <879559@stud.unive.it>
2021-12-03 20:59:45 +0100

    solving some problems with add.py

--- package/add.py
+++ package/add.py
@@ -1,4 +1,14 @@
+import pandas as pd
+import numpy as np
+
+
+
+
+
+
+
+
 def add_col(col):
-    df = pd.read_csv(("data.csv"), index_col = O, dtype = str))
+    df = pd.read_csv(("data.csv"), index_col = O, dtype = str)
     df[col] = np.nan
     df.to_csv("data.csv")
