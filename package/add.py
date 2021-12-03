da63904a9c01f57173c854bd09a818ac12b13ed4
albertocastelli <879559@stud.unive.it>
2021-12-03 21:02:26 +0100

    solving some problems with add.py

--- package/add.py
+++ package/add.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 def add_book(book):
     df = pd.read_csv(("data.csv"), index_col = O, dtype = str)
     df.loc[len(df.index)] = [book] + [np.nan for x in range(len(df.loc[0])-1)]
