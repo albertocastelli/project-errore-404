14bc0ca638b13bc88fa9d5db424b71ad3cf50fc8
albertocastelli <879559@stud.unive.it>
2021-12-05 19:05:44 +0100

    adding argparse for add_book and add_col to the main

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-2+2
+from package.add import add_book
+from package.add import add_col
+from package.add import add_change_value
+import argparse
+import pandas as pd
+
+df = pd.read_csv("data.csv", index_col = 0, dtype = str)
+
+fun={"1":add_book,
+"2":add_col,
+}
+
+
+parser=argparse.ArgumentParser(description="Handle the dataset of a library")
+parser.add_argument("command",type=str, help="insert a valid number: '1' for adding a book,"
+ "'2' for adding a column,"
+, choices=fun.keys())
+parser.add_argument("--n_book", type = str, help = "insert a string with the name of the new book")
+parser.add_argument("--n_col", type=str, help = "insert a string with the name of the new column")
+
+args=parser.parse_args()
+
+f=fun[args.command]
+
+if f==add_book:
+  f(args.n_book)
+
+elif f==add_col:
+  f(args.n_col)
+
