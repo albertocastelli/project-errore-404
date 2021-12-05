6514c474f153e7e9ca55561c52cd41fe0a2d37ca
FrancescaBuoso <94048997+FrancescaBuoso@users.noreply.github.com>
2021-12-05 13:45:29 +0100

    Update show.py

--- package/show.py
+++ package/show.py
@@ -1,3 +1,7 @@
 def show_database():
     df=pd.read_csv("data.csv", index_col = 0, dtype = str)
     print(df)
+
+    def top_writers():
+        df = pd.read_csv("data.csv", index_col=0, dtype=str)
+       
