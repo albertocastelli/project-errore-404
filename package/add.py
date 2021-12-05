f203c23687736d33852b229fc40aa72a0057c657
albertocastelli <879559@stud.unive.it>
2021-12-05 18:38:56 +0100

    putting every add funciton together

--- package/add.py
+++ package/add.py
@@ -1,3 +1,19 @@
+import pandas as pd
+import numpy as np
+
+
+def add_col(col):
+    df = pd.read_csv(("data.csv"), index_col = 0, dtype = str)
+    df[col] = np.nan
+    df.to_csv("data.csv")
+
+
+def add_book(book):
+    df = pd.read_csv(("data.csv"), index_col =0, dtype = str)
+    df.loc[len(df.index)] = [book] + [np.nan for x in range(len(df.loc[0])-1)]
+    df.to_csv("data.csv")
+
+
 def add_change_value(book, col, value, verbose = False):
     df = pd.read_csv("deta.csv", index_col = 0, dtype = str)
     if verbose:
@@ -6,3 +22,4 @@ def add_change_value(book, col, value, verbose = False):
         df.loc[df[df["book"]==book].index.tolist()[0], col] = value
         df.to_csv("data.csv")
         print("changes have been made")
+
