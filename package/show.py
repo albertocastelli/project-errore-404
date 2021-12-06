e0919060b95230597ff2117c17216d5c91c9d574
FrancescaBuoso <94048997+FrancescaBuoso@users.noreply.github.com>
2021-12-06 14:16:08 +0100

    Update show.py

--- package/show.py
+++ package/show.py
@@ -13,3 +13,15 @@ def show_database():
                 color = ["c", "orange", "c", "orange", "c"])
         plt.show()
         print(df.count.T)
+        
+         def top_genre():
+        df = pd.read_csv("data.csv", index_col=0, dtype=str)
+        #creating the dataframe
+        df_count = df.groupby("genre").count().sort_values(
+            "books", ascending = False).iloc[:5, :1]
+        #creating the plot
+        fig = plt.figure(figsize = (10,5))
+        plt.bar(x = df_count.index, height = df_count["books"],
+                color = ["c", "orange", "c", "orange", "c"])
+        plt.show()
+        
