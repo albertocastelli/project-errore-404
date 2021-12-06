09ed6a495c8d55d6a5ed82dd437fd8f176cc3f48
albertocastelli <879559@stud.unive.it>
2021-12-06 10:32:32 +0100

    eliminati verbose from add_change_value

--- package/add.py
+++ package/add.py
@@ -15,10 +15,8 @@ def add_book(book):
     df.to_csv("data.csv")
 
 
-def add_change_value(book, col, value, verbose = False):
+def add_change_value(book, col, value):
     df = pd.read_csv("deta.csv", index_col = 0, dtype = str)
-    if verbose:
-        print("we are making the changes you want...")
     if book in list(df["books"]) and col in list(df.columns):
         df.loc[df[df["book"]==book].index.tolist()[0], col] = value
         df.to_csv("data.csv")
