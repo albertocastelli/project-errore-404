285c12c47f408532cbe8d297bdeb7534f5a4b193
FrancescaBuoso <94048997+FrancescaBuoso@users.noreply.github.com>
2021-12-06 12:49:04 +0100

    Update show.py

--- package/show.py
+++ package/show.py
@@ -4,4 +4,12 @@ def show_database():
 
     def top_writers():
         df = pd.read_csv("data.csv", index_col=0, dtype=str)
-       
+        #creating the dataframe
+        df_count = df.groupby("writers").count().sort_values(
+            "books", ascending = False).iloc[:5, :1]
+        #creating the plot
+        fig = plt.figure(figsize = (10,5))
+        plt.bar(x = df_count.index, height = df_count["books"],
+                color = ["c", "orange", "c", "orange", "c"])
+        plt.show()
+        print(df.count.T)
