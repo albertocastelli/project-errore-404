fc99185666e20e3f81eb15e4cb9efd638c422f49
fragianca <94049686+fragianca@users.noreply.github.com>
2021-12-08 16:47:20 +0100

    Update add.py

--- package/add.py
+++ package/add.py
@@ -17,8 +17,20 @@ def add_book(book):
 def add_change_value(book, col, value):
     df = pd.read_csv("deta.csv", index_col = 0, dtype = str)
     if book in list(df["books"]) and col in list(df.columns):
-        df.loc[df[df["book"]==book].index.tolist()[0], col] = value
-        df.to_csv("data.csv")
-        print("changes have been made")
+        if isinstance(value,str):
+            if "," not in value:
+                df.loc[df[df["book"]==book].index.tolist()[0], col] = value
+                df.to_csv("data.csv")
+                return "changes have been made"
+            elif "," in value:
+                return "please, don't insert any comma ',' as new value"
+         elif not isinstance(value,str):
+            return "please insert a string with a valid new value"
+     elif book not in list(df["books"]):
+        return "please insert a book that is present in the dataframe"
+     elif col not in list(df.columns):
+            return "please insert a column present in the dataframe"
+     else:
+        return "please insert valid inputs"
 
 
