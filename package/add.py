8a3599ece374be13cabc4cf827aae6270caa7115
sarativ <94049698+sarativ@users.noreply.github.com>
2021-12-11 11:01:47 +0100

    Update add.py

--- package/add.py
+++ package/add.py
@@ -3,19 +3,29 @@ import numpy as np
 
 
 def add_col(col):
+    # open the data set as df
     df = pd.read_csv(("data.csv"), index_col = 0, dtype = str)
+    # take as input a new column and check if it is a string
     if isinstance(col, str):
+        # if it is a string, check if there are no commas 
         if "," not in col:
+            # if there are no commas, add the column to df
             if col in list (df.columns):
+                # if the column already exists in df, do not add it and display a message saying it already exists
                 return "this column is already present in the database"
+            # if there are no commas and the column is not in df yet, attach the new column to df
             else:
                 df[col] = np.nan
                 df.to_csv("data.csv")
+                # after attaching, return a message that confirms the changes
                 return "changes have been made"
+        # if there are commas in the column name, return a message saying not to insert commas
         elif "," in col:
             return "please, don't insert any comma ',' as new value"
+    #if the name of the new column is no a string, show a message saying that you have to insert a string as value
     elif not isinstance(col, str):
         return "please insert a string with a name for the new column"
+    # for other situations, display that the input is not valid
     else:
         return "please insert a valid input"
 
