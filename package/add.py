8ad1d6da39c7cc1f0efbc3e9116c8e3e31ccda67
sarativ <94049698+sarativ@users.noreply.github.com>
2021-12-11 11:21:31 +0100

    Update add.py

--- package/add.py
+++ package/add.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def add_col(col):
-    # open the data set as df
+    # open the dataset as df
     df = pd.read_csv(("data.csv"), index_col = 0, dtype = str)
     # take as input a new column and check if it is a string
     if isinstance(col, str):
@@ -47,19 +47,31 @@ def add_change_value(book, col, value):
 
 
 def add_book(n_book):
+    # open the data set as df
     df = pd.read_csv("data.csv", index_col = 0, dtype = str)
+    # take a new book title and check if it is a string
     if isinstance(n_book,str):
+        # if it is a string and there are no commas in the name
         if "," not in n_book:
+            # if the book title is alredy in df 
             if n_book in list(df["books"]):
+                # return a message saying that is it already in df
                 return "this book is already present in the database"
+            # if book title not in df yet
             else:
+                # use the book title as index of the column 0 'books' + insert nan to the other columns (ie writers, genre)
                 df.loc[len(df.index)] = [n_book] + [np.nan for x in range(len(df.loc[0]) - 1)]
+                # attach changes to csv and return message of changes succefully made
                 df.to_csv("data.csv")
                 return "changes have been made"
+        # if there are commas in the book title
         elif "," in n_book:
+            # return the following message
             return "please, don't insert any comma ',' as new value"
+    # if the input is not a string, return a message saying to insert a string as input
     elif not isinstance(n_book,str):
         return "please insert a string with a name for the new book"
+    # for other situations, display that the input is not valid
     else:
         return "please insert a valid input"
 
