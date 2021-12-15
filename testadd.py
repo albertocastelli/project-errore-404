d04b623b01f4b8f63099ca63be65f3bcdb3a8bff
sarativ <94049698+sarativ@users.noreply.github.com>
2021-12-15 15:55:13 +0100

    Update testadd.py

--- testadd.py
+++ testadd.py
@@ -3,10 +3,26 @@ from package.add import add_col
 from package.add import add_book
 from package.add import add_change_value
 
-
+"""
+Unit tests to verify how the code responds to given inputs that are known to be valid, invalid and already in the dataset.
+The tests have been created just for the functions that require to enter an input because they directly modify the database which is csv and a problem arises when a value 
+containing a comma is inputted and saved since the csv consider a comma as a value separator. Therefore, if a value containing a comma is inputted and saved,
+the csv file will not be correctly opened anymore.
+There are three tests done one add_column, add_book and add_change_value each time. Those tests are:
+- the test_correct_values that checks if the known valid entries are accepted and returns a "changes have been made" message when the test is passed;
+- the test_wrong_values that checks if the known invalid errors (those containing a comma) are not accepted as expected. If the tests is passed, a "please, don't insert any comma ',' 
+  as new value" message is returned.
+- as corner case, the test_already_present_values function that checks if an entry is already in the database and returns a message accordingly: 
+  for add_column, if the column we want to add already exits in the database, a "this column is already present in the database" message has to be displayed;
+  for add_book, if the book we want to add is already saved, a "this book is already present in the database" has to be displayed.
+"""
+# create a class
 class TestInput(unittest.TestCase):
 
+    # create the module to test known valid entries
     def test_correct_values(self):
+        
+        
         self.assertEqual(add_col("copies_sold"), "changes have been made")
         self.assertEqual(add_book("a game of thrones"), "changes have been made")
         self.assertEqual(add_change_value("divine comedy", "publication_date", "1472"), "changes have been made")
